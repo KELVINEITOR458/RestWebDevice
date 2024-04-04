7be130f06a681de5b35bfb6ffcb2d5631fabfc44
KELVINEITOR458 <kelvimnbermeo8@gmail.com>
2024-04-04 07:54:25 -0500

    Acabe el modulo 2 chavales

--- WorkSpaceKelvin/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ WorkSpaceKelvin/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -1,14 +1,19 @@
 package com.krakedev.servicios;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.krakedev.entidades.Cliente;
+import com.krakedev.excepciones.KrakeDevException;
 import com.krakedev.persistencia.ClientesBDD;
 
 @Path("clientes")
@@ -33,17 +38,79 @@ public class ServicioClientes {
 	@Path("insertar")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void insertar(Cliente cliente) {
+	public Response insertar(Cliente cliente) {
 		System.out.println(">>>>>>>" +cliente);
 		ClientesBDD cli = new ClientesBDD();
-		cli.insertar(cliente);
+		try {
+			cli.insertar(cliente);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 	
 	@Path("act")
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void actualizar(Cliente cliente) {
+	public Response actualizar(Cliente cliente) {
 		System.out.println("ACTUALIZANDOCLIENTE>>>>>>>" +cliente);
+		ClientesBDD cli = new ClientesBDD();
+		try {
+			cli.actualizar(cliente);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@Path("all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response obtenerClientes(){
+		ClientesBDD cli = new ClientesBDD();
+		ArrayList<Cliente> clientes = null;
+		try {
+			clientes = cli.recuperarTodos();
+			return Response.ok(clientes).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@Path("buscarPorCedula/{cedulaParam}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorCedula(@PathParam("cedulaParam") String cedula){
+		ClientesBDD cli = new ClientesBDD();
+		Cliente cliente = null;
+		try {
+			cliente = cli.buscarPorPK(cedula);
+			return Response.ok(cliente).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@Path("buscarPorHijos/{hijosParam}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorHijos(@PathParam("hijosParam") int cantidadHijos){
+		ClientesBDD cli = new ClientesBDD();
+		ArrayList<Cliente> clientes = null;
+		try {
+			clientes = cli.retornarPorHijos(cantidadHijos);
+			return Response.ok(clientes).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 	
 }
